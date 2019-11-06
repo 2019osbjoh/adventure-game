284d4f2354900921bf3a4a1600718071b8db6029
2019osbjoh <john.osborne151@gmail.com>
2019-11-06 19:41:44 +0000

    ~

---
+++ TEXTBASEDGAME.py
@@ -0,0 +1,12 @@
+#print("Text based adventure")
+
+# what we plan to do:
+
+# player = @ symbol
+# walls are -, _, /, \ or |
+# you can't hit the walls
+# secret rooms (sorcerers cave sytle) you can't see the rooms until you move into them
+# ? = key - you won't see the door until you pick up the key
+# the keys are colour coded for certain doors and the key might ot correspond to the door of the room you are in
+
+
